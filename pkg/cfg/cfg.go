e36d601e1edc5a49fc834c82902f3e7ee76d5569
Milos Dukic <milos@dekadencija.rs>
2025-03-11 15:09:14 +0100

    pkg/cfg: added cfg.go

---
+++ pkg/cfg/cfg.go
@@ -0,0 +1,23 @@
+package cfg
+
+import (
+	"context"
+	"log"
+
+	"github.com/aws/aws-sdk-go-v2/config"
+)
+
+func InitiateClient(r string) (aws.Config, error) {
+	ctx := context.TODO()
+
+	cfg, err := config.LoadDefaultConfig{
+		ctx,
+		config.WithRegion(r),
+	}
+
+	if err != nil {
+		log.Fatal("Failed to initialize AWS client.")
+	}
+
+	return cfg, err
+}
