624b3162a15c4fab09ca22a954aeb88a4b1567c8
Milos Dukic <milos@dekadencija.rs>
2025-03-11 15:34:26 +0100

    pkg/cfg/cfg.go: fix typo

--- pkg/cfg/cfg.go
+++ pkg/cfg/cfg.go
@@ -4,16 +4,17 @@ import (
 	"context"
 	"log"
 
+	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
 )
 
 func InitiateClient(r string) (aws.Config, error) {
 	ctx := context.TODO()
 
-	cfg, err := config.LoadDefaultConfig{
+	cfg, err := config.LoadDefaultConfig(
 		ctx,
 		config.WithRegion(r),
-	}
+	)
 
 	if err != nil {
 		log.Fatal("Failed to initialize AWS client.")
